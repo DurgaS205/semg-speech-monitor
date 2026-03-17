1d7d06ecbe49f0ec5a26b5eb91a170b75856db42
adiko-704 <aditapnair@gmail.com>
2026-03-17 22:21:57 +0530

    Delete emg_generator.py

--- emg_generator.py
+++
@@ -1,9 +0,0 @@
-import random
-import time
-
-def generate_emg_data():
-    """Simulate EMG signal values"""
-    while True:
-        value = random.randint(1500, 2500)
-        yield value
-        time.sleep(0.1)
